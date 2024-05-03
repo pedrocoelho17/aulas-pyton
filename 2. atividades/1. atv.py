57046c8cdec0fa21f97cbc2f619d7a314b0d4574
pedrocoelho17 <161032195+pedrocoelho17@users.noreply.github.com>
2024-05-03 14:25:31 +0000

    media, soma, produto

--- 2. atividades/1. atv.py
+++ 2. atividades/1. atv.py
@@ -5,13 +5,16 @@ nome = input("Digite seu nome: ")
 idade = int(input("Digite sua idade: "))
 nota1 = float(input("Digite a 1ª nota: "))
 nota2 = float(input("Digite a 2ª nota: "))
-media = (nota1 + nota2) / 2
+nota3 = float(input("Digite a 3ª nota: "))
+media = (nota1 + nota2 + nota3) / 3
 
 
 os.system("cls||clear")
 print(f"Exibindo resultados")
 print(f"Nome: {nome}")
 print(f"Idade: {idade}")
-print(f"1ª nota: {nota1}")
-print(f"2ª nota: {nota2}")
 print(f"Média: {media}")
+if media >= 7:
+    print("Aprovado!")
+else:
+    print(f"Reprovado!")
