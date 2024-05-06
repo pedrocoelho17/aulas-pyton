837919ebefc36cb888359fe621bf94e8ef48d306
pedrocoelho17 <161032195+pedrocoelho17@users.noreply.github.com>
2024-05-06 14:11:54 +0000

    aula 2

---
+++ 3. Condicionais /atv 5.py
@@ -0,0 +1,12 @@
+import os
+os.system("cls||clear")
+
+login = input("Digite seu login: ")
+senha = input("Digite sua senha: ")
+
+
+
+if login == "peu17" and senha == "peu123":
+    print("Bem-vindo!")
+else:
+    print("Login ou login incorretos")    
---
+++ 3. Condicionais /atv idade para votar.py
@@ -0,0 +1,9 @@
+import os
+os.system("cls||clear")
+
+idade = int(input("Digite sua idade: "))
+
+if idade >= 18 and idade < 65:
+    print("Voto obrigatório.")
+else:
+    print("Voto não obrigatório")    
